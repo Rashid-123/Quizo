408a6cb7269138a36032911a9ef0283a8eecd2ad
Rashid <shadanrashid786@gmail.com>
2025-02-15 18:30:21 +0530

    icon error

--- frontend/src/config/app.ts
+++ frontend/src/config/app.ts
@@ -1,23 +1,23 @@
-// interface AppConfig {
-//     name: string,
-//     github: {
-//         title: string,
-//         url: string
-//     },
-//     author: {
-//         name: string,
-//         url: string
-//     },
-// }
+interface AppConfig {
+  name: string;
+  github: {
+    title: string;
+    url: string;
+  };
+  author: {
+    name: string;
+    url: string;
+  };
+}
 
-// export const appConfig: AppConfig = {
-//     name: "Shadcn Sample",
-//     github: {
-//         title: "Shadcn Sample",
-//         url: "https://github.com/hayyi2/shadcn-sample",
-//     },
-//     author: {
-//         name: "hayyi",
-//         url: "https://github.com/hayyi2/",
-//     }
-// }
+export const appConfig: AppConfig = {
+  name: "Shadcn Sample",
+  github: {
+    title: "Shadcn Sample",
+    url: "https://github.com/hayyi2/shadcn-sample",
+  },
+  author: {
+    name: "hayyi",
+    url: "https://github.com/hayyi2/",
+  },
+};
--- frontend/src/config/menu.ts
+++ frontend/src/config/menu.ts
@@ -1,147 +1,146 @@
-// import { Icons } from "@/components/icons"
+import { Icons } from "@/components/icons";
 
-// interface NavItem {
-//     title: string
-//     to?: string
-//     href?: string
-//     disabled?: boolean
-//     external?: boolean
-//     icon?: keyof typeof Icons
-//     label?: string
-// }
+interface NavItem {
+  title: string;
+  to?: string;
+  href?: string;
+  disabled?: boolean;
+  external?: boolean;
+  icon?: keyof typeof Icons;
+  label?: string;
+}
 
-// interface NavItemWithChildren extends NavItem {
-//     items?: NavItemWithChildren[]
-// }
+interface NavItemWithChildren extends NavItem {
+  items?: NavItemWithChildren[];
+}
 
-// export const mainMenu: NavItemWithChildren[] = [
-//     {
-//         title: 'Dashboard',
-//         to: '/',
-//     },
-//     {
-//         title: 'Layouts',
-//         items: [
-//             {
-//                 title: 'Topnav Layout',
-//                 to: '/layout/topnav',
-//             },
-//             {
-//                 title: 'Sidenav Layout',
-//                 to: '/layout/sidenav',
-//             },
-//         ],
-//     },
-//     {
-//         title: 'Elements',
-//         items: [
-//             {
-//                 title: 'Forms',
-//                 to: '/forms',
-//             },
-//             {
-//                 title: 'Tables',
-//                 to: '/table',
-//             },
-//             {
-//                 title: 'Widgets',
-//                 to: '/widgets',
-//             },
-//             {
-//                 title: 'More Components',
-//             },
-//             {
-//                 title: 'Components',
-//                 to: '/components',
-//             },
-//         ],
-//     },
-//     {
-//         title: 'Pages',
-//         items: [
-//             {
-//                 title: 'Empty',
-//                 to: '/empty',
-//             },
-//             {
-//                 title: 'Error',
-//                 to: '/error',
-//             },
-//             {
-//                 title: 'Auth Pages',
-//             },
-//             {
-//                 title: 'Login',
-//                 to: '/auth/login',
-//             },
-//             {
-//                 title: 'Register',
-//                 to: '/auth/register',
-//             },
-//         ],
-//     },
-// ]
+export const mainMenu: NavItemWithChildren[] = [
+  {
+    title: "Dashboard",
+    to: "/",
+  },
+  {
+    title: "Layouts",
+    items: [
+      {
+        title: "Topnav Layout",
+        to: "/layout/topnav",
+      },
+      {
+        title: "Sidenav Layout",
+        to: "/layout/sidenav",
+      },
+    ],
+  },
+  {
+    title: "Elements",
+    items: [
+      {
+        title: "Forms",
+        to: "/forms",
+      },
+      {
+        title: "Tables",
+        to: "/table",
+      },
+      {
+        title: "Widgets",
+        to: "/widgets",
+      },
+      {
+        title: "More Components",
+      },
+      {
+        title: "Components",
+        to: "/components",
+      },
+    ],
+  },
+  {
+    title: "Pages",
+    items: [
+      {
+        title: "Empty",
+        to: "/empty",
+      },
+      {
+        title: "Error",
+        to: "/error",
+      },
+      {
+        title: "Auth Pages",
+      },
+      {
+        title: "Login",
+        to: "/auth/login",
+      },
+      {
+        title: "Register",
+        to: "/auth/register",
+      },
+    ],
+  },
+];
 
-// export const sideMenu: NavItemWithChildren[] = [
-
-//     {
-//         title: 'Dashboard',
-//         to: '/',
-//     },
-//     {
-//         title: 'Layouts',
-//         items: [
-//             {
-//                 title: 'Topnav Layout',
-//                 to: '/layout/topnav',
-//             },
-//             {
-//                 title: 'Sidenav Layout',
-//                 to: '/layout/sidenav',
-//             },
-//         ],
-//     },
-//     {
-//         title: 'Elements UI',
-//     },
-//     {
-//         title: 'Forms',
-//         to: '/forms',
-//     },
-//     {
-//         title: 'Tables',
-//         to: '/table',
-//     },
-//     {
-//         title: 'Widgets',
-//         to: '/widgets',
-//     },
-//     {
-//         title: 'Components',
-//         to: '/components',
-//     },
-//     {
-//         title: 'Extra UI',
-//     },
-//     {
-//         title: 'Auth Pages',
-//         items: [
-//             {
-//                 title: 'Login',
-//                 to: '/auth/login',
-//             },
-//             {
-//                 title: 'Register',
-//                 to: '/auth/register',
-//             },
-//         ],
-//     },
-//     {
-//         title: 'Empty Page',
-//         to: '/empty',
-//     },
-//     {
-//         title: 'Error Page',
-//         to: '/error',
-//     },
-// ]
+export const sideMenu: NavItemWithChildren[] = [
+  {
+    title: "Dashboard",
+    to: "/",
+  },
+  {
+    title: "Layouts",
+    items: [
+      {
+        title: "Topnav Layout",
+        to: "/layout/topnav",
+      },
+      {
+        title: "Sidenav Layout",
+        to: "/layout/sidenav",
+      },
+    ],
+  },
+  {
+    title: "Elements UI",
+  },
+  {
+    title: "Forms",
+    to: "/forms",
+  },
+  {
+    title: "Tables",
+    to: "/table",
+  },
+  {
+    title: "Widgets",
+    to: "/widgets",
+  },
+  {
+    title: "Components",
+    to: "/components",
+  },
+  {
+    title: "Extra UI",
+  },
+  {
+    title: "Auth Pages",
+    items: [
+      {
+        title: "Login",
+        to: "/auth/login",
+      },
+      {
+        title: "Register",
+        to: "/auth/register",
+      },
+    ],
+  },
+  {
+    title: "Empty Page",
+    to: "/empty",
+  },
+  {
+    title: "Error Page",
+    to: "/error",
+  },
+];
